44aefb10ec11860503952e6300d0fb29624a08aa
Simone Capuano <scapuano19@gmail.com>
2021-05-07 11:16:16 +0200

    I did work mainly on setting state and handling state changes in SearchBar.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
 
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
   render() {
     return (
     <div className="App">
       <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList />
+      <SearchBar searchYelp={this.searchYelp}/>
+      <BusinessList businesses={businesses}/>
     </div>
       );
   }
