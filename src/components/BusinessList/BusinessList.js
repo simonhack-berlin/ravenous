8b266aba983a7a5b2664657f2536b646c7d98b9b
Simone Capuano <scapuano19@gmail.com>
2021-05-07 14:41:51 +0200

    Interacting with the Yelp API

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './BusinessList.css';
+
 import Business from '../Business/Business';
 
-
-
-export default class BusinessList extends Component {
-    render() {
-        return (
-            <div className="BusinessList">
-            {this.props.businesses.map((business) => {
-                return (
-                    <Business business={business}/>
-                )
-            })}
-            </div>
-        )
-    }
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} key={business.id} />
+          })
+        }
+      </div>
+    );
+  }
 }
- 
+
+export default BusinessList;
